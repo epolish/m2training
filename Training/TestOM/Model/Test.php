5f625b567b92bd672021bb19bd417c050a66ef76
Eugene Polischuk <e.polischuk@dinarys.com>
2019-10-17 21:42:26 +0300

    1.5

--- Training/TestOM/Model/Test.php
+++ Training/TestOM/Model/Test.php
@@ -8,17 +8,20 @@ class Test
     private $arrayList;
     private $name;
     private $number;
+    private $managerFactory;
 
     public function __construct(
         \Training\TestOM\Model\ManagerInterface $manager,
         $name,
         int $number,
-        array $arrayList
+        array $arrayList,
+        \Training\TestOM\Model\ManagerInterfaceFactory $managerFactory
     ) {
         $this->manager = $manager;
         $this->name = $name;
         $this->number = $number;
         $this->arrayList = $arrayList;
+        $this->managerFactory = $managerFactory;
     }
 
     public function log()
@@ -31,6 +34,9 @@ class Test
         print_r($this->number);
         echo '<br>';
         print_r($this->arrayList);
+        echo '<br>';
+        $newManager = $this->managerFactory->create();
+        print_r(get_class($newManager));
         echo '</pre>';
     }
 }
