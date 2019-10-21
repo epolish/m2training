51940c6989ad307e6a2a9f5fd970d5e2d103ad4a
Eugene Polischuk <e.polischuk@dinarys.com>
2019-10-21 22:31:26 +0300

    1.7 clean commit

--- Training/Test/Plugin/Model/Url.php
+++ Training/Test/Plugin/Model/Url.php
@@ -9,8 +9,6 @@ class Url
         $routePath = null,
         $routeParams = null
     ) {
-        $test = 1;
-        $test = 2;
         if ($routePath == 'customer/account/create') {
             return ['customer/account/login', null];
         }
